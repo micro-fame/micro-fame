28d2bdde57e5ee8a95444450a596057a987c04c9
saggiyogesh <saggiyogesh@gmail.com>
2017-06-19 00:08:21 +0530

    some User methods moved to BaseWithoutRemote

--- test/services/BaseWithoutRest.js
+++ test/services/BaseWithoutRest.js
@@ -13,6 +13,36 @@ class BaseWithoutRest {
     const { app: { services: { User } } } = this;
     return User;
   }
+
+  @Remote({
+    path: '/get-with-params/:name/:text'
+  })
+  async getWithParams(name, text) {
+    return {
+      name,
+      text
+    };
+  }
+
+  @Remote({
+    path: '/paramsFromQuery',
+    args: {
+      text: ({ query: { text } }) => text,
+      number: ({ query: { number } }) => number
+    }
+  })
+  async paramsFromQuery(text, number) {
+    return { text, number };
+  }
+
+  @Remote({
+    path: '/argsGetterBody',
+    argsGetter: 'body',
+    method: 'post'
+  })
+  async argsGetterBody(name, text) {
+    return { text, name };
+  }
 };
 
 module.exports = BaseWithoutRest;
--- test/services/User.js
+++ test/services/User.js
@@ -29,32 +29,11 @@ class User extends BaseWithoutRest {
   }
 
   @Remote({
-    path: '/get-with-params/:name/:text'
-  })
-  async getWithParams(name, text) {
-    return {
-      name,
-      text
-    };
-  }
-
-  @Remote({
     path: '/getOptionalParam(/:text)',
     method: 'get'
   })
   async getOptionalParam(text) {
     return { text: text };
-  }
-
-  @Remote({
-    path: '/paramsFromQuery',
-    args: {
-      text: ({ query: { text } }) => text,
-      number: ({ query: { number } }) => number
-    }
-  })
-  async paramsFromQuery(text, number) {
-    return { text, number };
   }
 
   @Remote({
@@ -98,15 +77,6 @@ class User extends BaseWithoutRest {
     argsGetter: 'query'
   })
   async argsGetterQuery(name, text) {
-    return { text, name };
-  }
-
-  @Remote({
-    path: '/argsGetterBody',
-    argsGetter: 'body',
-    method: 'post'
-  })
-  async argsGetterBody(name, text) {
     return { text, name };
   }
 
