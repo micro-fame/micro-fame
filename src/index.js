78bf141209fe53ae7de0419c046edcca00864244
saggiyogesh <saggiyogesh@gmail.com>
2017-11-11 01:04:15 +0530

    expose utils

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const Boot = require('./lib/Boot');
 const RestService = require('./lib/decorators/RestService');
 const Remote = require('./lib/decorators/Remote');
+const fsUtil = require('./utils/fsUtil');
+const jsUtil = require('./utils/jsUtil');
+const objectUtil = require('./utils/objectUtil');
+
 
 exports.Boot = Boot;
 exports.RestService = RestService;
 exports.Remote = Remote;
+
+exports.Utils = {
+  fs: fsUtil,
+  js: jsUtil,
+  object: objectUtil
+};
